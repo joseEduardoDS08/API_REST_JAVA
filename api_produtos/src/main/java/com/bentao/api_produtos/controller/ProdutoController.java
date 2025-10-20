cf1a8a96c40e93923924834c48517b618e67b677
joseEduardoDS08 <je3751907@gmail.com>
2025-10-20 10:19:21 -0300

    Add files via upload

---
+++ api_produtos/src/main/java/com/bentao/api_produtos/controller/ProdutoController.java
@@ -0,0 +1,52 @@
+package com.bentao.api_produtos.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.bentao.api_produtos.models.Produto;
+import com.bentao.api_produtos.repository.ProdutoRepository;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
+
+    private final ProdutoRepository repository;
+
+    public ProdutoController(ProdutoRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    public List<Produto> listarTodos() {
+        return repository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public Produto buscarPorId(@PathVariable Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @PostMapping
+    public Produto criar(@RequestBody Produto produto) {
+        return repository.save(produto);
+    }
+
+    @PutMapping("/{id}")
+    public Produto atualizar(@PathVariable Long id, @RequestBody Produto produto) {
+        produto.setId(id);
+        return repository.save(produto);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deletar(@PathVariable Long id) {
+        repository.deleteById(id);
+    }
+}
